03c90d339dd381181f6153996d4199bcba78b705
Systho <systho@gmail.com>
2018-09-14 13:45:11 +0200

    make config work on both dev and production

--- src/server/app.js
+++ src/server/app.js
@@ -1,17 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const projectRoot = path.join(__dirname, '../..');
+const serverRoot = path.join(__dirname, '.');
+
+const app = express();
 
 // view engine setup
-app.locals['assetsBaseUrl'] = '//localhost:3000';
+if (fs.existsSync(path.join(projectRoot, 'dist'))) {
+  app.locals.assetsBaseUrl = '';
+} else {
+  app.locals.assetsBaseUrl = '//localhost:3000';
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,24 +28,27 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(
+  sassMiddleware({
+    src: path.join(serverRoot, 'public'),
+    dest: path.join(serverRoot, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
